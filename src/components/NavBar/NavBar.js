f6d51f9eba2cb9e2e623e203f7206179d9a4cf4d
Tomás Payer <91624465+TomasPayer@users.noreply.github.com>
2022-02-09 20:59:16 -0300

    NavBar created

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return ( <div class="NavBar">
+    <a class="active" href="#home">Home</a>
+    <a href="#news">Phones</a>
+    <a href="#contact">TV</a>
+    <a href="#about">Other Accessories</a>
+  </div>
+    )
+}
+
+
+
+export default NavBar
